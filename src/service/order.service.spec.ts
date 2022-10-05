f322d995d4b360c64613746ab2893b8ce2a7263b
Daniel Hessel <daniel@sharbe.com.br>
2022-10-05 20:39:20 -0300

    feat: implement rewards program

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,19 @@
+import { Customer } from "../entity/customer";
 import { Order } from "../entity/order";
 import { OrderItem } from "../entity/order-item";
 import { OrderService } from "./order.service";
 
 describe("Order service unit tests", () => {
+  it("should place an order", () => {
+    const customer = new Customer("C001", "Customer 1");
+    const item = new OrderItem("I001", "Item 1", 10, "P001", 1);
+
+    const order = OrderService.placeOrder(customer, [item]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+
   it("should get total of all orders", () => {
     const item1 = new OrderItem("I001", "Item 1", 100, "P001", 1);
     const item2 = new OrderItem("I002", "Item 2", 200, "P002", 2);
